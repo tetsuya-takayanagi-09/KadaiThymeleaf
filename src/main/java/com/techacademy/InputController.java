242bc7be5d02ce2154c2c23fffdf9a915ff12a1e
tetsuya-takayanagi-09 <tetsuya.takayanagi09@gmail.com>
2026-01-03 10:06:01 +0900

    first commit

---
+++ src/main/java/com/techacademy/InputController.java
@@ -0,0 +1,23 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class InputController {
+
+    @GetMapping("/input")
+    public String getForm(@RequestParam(value="previous", required=false) String previous, Model model) {
+        model.addAttribute("previous", previous);
+        return "input";
+    }
+
+    @PostMapping("/output")
+    public String postForm(@RequestParam("val") String val, Model model) {
+        model.addAttribute("val", val);
+        return "output";
+    }
+}
